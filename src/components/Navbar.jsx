80febeb0bc3920a3d90a9bdc258a2b933a28abf0
Imani-Maua <elainewalter7022@gmail.com>
2026-02-10 23:23:10 +0100

    Toggle navbar on small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
         <nav className={styles.navbar}>
-            <Link to="/" className={styles.brandName}>
+            <Link to="/" className={styles.brandName} onClick={closeMenu}>
                 maua.imani
             </Link>
 
@@ -15,14 +21,32 @@ function Navbar() {
             </div>
 
             <div className={styles.contactCard}>
-                <a href="mailto:elainemaua644@gmail.com?subject=We are looking for a software engineer&body=Hello Maua, "><button>
-                    Contact Me
-                </button>
+                <a href="mailto:elainemaua644@gmail.com?subject=We are looking for a software engineer&body=Hello Maua, ">
+                    <button>Contact Me</button>
                 </a>
             </div>
 
+            <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
+                🌸
+            </div>
+
+            {isMenuOpen && (
+                <div className={styles.mobileOverlay}>
+                    <div className={styles.overlayContent}>
+                        <div className={styles.closeIcon} onClick={toggleMenu}>✕</div>
+                        <div className={styles.mobileLinks}>
+                            <NavLink to='/experience' onClick={closeMenu} className={styles.navLink}>Experience</NavLink>
+                            <NavLink to='/projects' onClick={closeMenu} className={styles.navLink}>Projects</NavLink>
+                            <NavLink to='/extracurriculars' onClick={closeMenu} className={styles.navLink}>Extracurriculars</NavLink>
+                            <a href="mailto:elainemaua644@gmail.com" className={styles.mobileContactLink} onClick={closeMenu}>
+                                Contact Me
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            )}
         </nav>
-    )
+    );
 }
 
 export default Navbar;
